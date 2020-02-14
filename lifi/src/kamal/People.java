8e2f1bcec09f049e2b4c26b765d5430e0f9871ef
Jayaraman <Jayaraman@192.168.2.151>
2020-02-14 13:52:15 +0530

    Interface

---
+++ lifi/src/kamal/People.java
@@ -0,0 +1,26 @@
+package kamal;
+
+public class People implements Rule {
+	void vote() {
+		System.out.println();
+	}
+
+	@Override
+	public void getmark() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void follow(int a) {
+		System.out.println(k);
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		People p=new People();
+		p.follow(10);
+		
+	}
+
+}
---
+++ lifi/src/kamal/Rule.java
@@ -0,0 +1,9 @@
+package kamal;
+
+public interface Rule {
+	int k=5;
+	void getmark();
+	void follow(int a);
+	
+
+}
