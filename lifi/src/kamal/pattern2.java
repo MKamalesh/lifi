1266cb9fc1c420406dbd15316ee5d9f79837c31b
Jayaraman <Jayaraman@192.168.2.151>
2020-02-14 13:51:00 +0530

    Pattern

---
+++ lifi/src/kamal/pattern2.java
@@ -0,0 +1,23 @@
+package kamal;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		int i=1,x=1,r=3,c=3;
+		while(i<=r)
+		{
+			int j=1;
+			while(j<=c)
+			{
+				System.out.print(x+" ");
+				x++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
