ad307e87c139a343233144244b88d20705ca3f18
Jayaraman <Jayaraman@192.168.2.151>
2020-02-14 13:50:08 +0530

    Abstract

---
+++ lifi/src/kamal/Example2.java
@@ -0,0 +1,19 @@
+package kamal;
+
+public class Example2 extends example {
+	void value(int a)
+	{
+		this.a=a;
+	}
+	Example2()
+	{
+		super(10);
+	}
+
+	public static void main(String[] args) {
+		Example2 b=new Example2();
+		b.add();// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lifi/src/kamal/example.java
@@ -0,0 +1,15 @@
+package kamal;
+
+public abstract class example {
+	int a=5;
+	void add()
+	{
+		System.out.println(a);
+	}
+	abstract void value(int a);
+	example(int a)
+	{
+		System.out.println("hello");
+	}
+
+}
